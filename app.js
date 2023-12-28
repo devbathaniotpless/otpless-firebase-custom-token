25346bc42185ebb6044e5fec1a1522e776b41f27
devbathani <bathanid888@gmail.com>
2023-12-28 14:33:40 +0530

    Base code

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const admin = require('firebase-admin');
+const serviceAccount = require('./serviceAccountKey.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+});
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+
+app.get('/auth', (req, res) => {
+    const phoneNumber = req.query.phoneNumber;
+    console.log("number " + phoneNumber);
+    admin.auth().getUserByPhoneNumber(`+${phoneNumber}`)
+        .then((userRecord) => {
+            res.json({ uid: userRecord.uid, phoneNumber: `+${phoneNumber}` });
+        })
+        .catch((error) => {
+            console.log('Error fetching user data:', error);
+            admin.auth().createUser({
+                phoneNumberVerified: true,
+                phoneNumber: `+${phoneNumber}`,
+                providerData: [{
+                    phoneNumber: `+${phoneNumber}`,
+                    providerId: 'phone',
+                }],
+
+            })
+                .then((userRecord) => {
+                    // See the UserRecord reference doc for the contents of userRecord.
+                    res.json({ uid: userRecord.uid, phoneNumber: `+${phoneNumber}` });
+                })
+                .catch((error) => {
+                    console.log('Error creating new user:', error);
+                });
+        });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
